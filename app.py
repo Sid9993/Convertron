636bde848631ef4afcd7632a7284daafc369012a
anandhu-eng <anandhukicks@gmail.com>
2021-06-08 12:57:02 +0530

    enhanced the flow between the pages

--- app.py
+++ app.py
@@ -3,16 +3,27 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return render_template('index.html')
 
+@app.route('/contact')
+def contact():
+   return render_template('contact.html')
 
+@app.route('/about')
+def about():
+   return render_template('about.html')
+
+@app.route('/convert')
+def convert():
+   return render_template('convert.html')
+   
 #upload function need completion
-@app.route('/upload')
+@app.route('/ddd')
 def upload_file():
-   return render_template('upload.html')
+   return render_template('index.html')
 	
 @app.route('/uploader', methods = ['GET', 'POST'])
-def upload_file():
+def uploads_file():
    if request.method == 'POST':
       f = request.files['file']
       #f.save(secure_filename(f.filename))
